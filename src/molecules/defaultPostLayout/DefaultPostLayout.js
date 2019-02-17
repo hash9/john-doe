fa83691cb7c44ab3f3b22c9db56d8d99e54ef7c2
hash9 <hprajapati9495@gmail.com>
2019-02-17 15:31:10 +0530

    theme obj destructured

--- src/molecules/defaultPostLayout/DefaultPostLayout.js
+++ src/molecules/defaultPostLayout/DefaultPostLayout.js
@@ -34,87 +34,87 @@ class DefaultPostLayout extends Component {
 	}
 
 	render() { 
-		const { postTitle, postDate, mainContent, handleBlogName } = this.props;
+    const { postTitle, postDate, mainContent, handleBlogName } = this.props;
 		
-		return (
+    return (
 			<ThemeContext.Consumer>
-				{theme => { 
+        {theme => { 
           const { link, mainTitle, hXl, content } = theme.config;
             return(
-						<Page>
-							<Page.Header className={styles.dPageHeader}>
-								<header>
-									<HeaderR text={'SpaceH'} onClick={handleBlogName} />
-								</header>
-							</Page.Header>
-							<Page.Body className={styles.dPageBody}>
-								<main>
-									<article>
+            <Page>
+              <Page.Header className={styles.dPageHeader}>
+                <header>
+                  <HeaderR text={'SpaceH'} onClick={handleBlogName} />
+                </header>
+              </Page.Header>
+              <Page.Body className={styles.dPageBody}>
+                <main>
+                  <article>
 
-										<header>
-											<div className={styles.mainContainer}>
+                    <header>
+                      <div className={styles.mainContainer}>
 
-												<div className={styles.titleContainer}>
-													<HeaderXL style={{ color: mainTitle }} text={postTitle} />
-												</div>
-				
-												<div className={styles.dateContainer}>
-													<small style={{ color: hXl }}>{postDate}</small>
-												</div>
+                        <div className={styles.titleContainer}>
+                          <HeaderXL style={{ color: mainTitle }} text={postTitle} />
+                        </div>
 
-											</div>
-										</header>
+                        <div className={styles.dateContainer}>
+                          <small style={{ color: hXl }}>{postDate}</small>
+                        </div>
 
-										<div className={styles.content}>
-											{mainContent}
-										</div>
+                      </div>
+                    </header>
 
-									</article>
-								</main>
-							</Page.Body>
-							<Page.Footer className={styles.dPageFooter}>
-								<footer>
-									<Seperator />
+                    <div className={styles.content}>
+                      {mainContent}
+                    </div>
 
-										<HeaderR text={'SpaceH'} onClick={handleBlogName} />
-										<div className={styles.footerMainContainer}>
-					
-											<div className={styles.footerLeftContainer}>
-													<Avatar image={require('../../assets/me.jpg')} size={'60px'}/>
-											</div>
-					
-											<div className={styles.footerRightContainer}>
-												<div className={styles.footerTopTextStyle}>
-													<p style={{ color: content }}>
-														Personal Blog by <Anchor href={''} style={{ color: link }} text={'Harshit Prajapati'} />
-													</p>
-													<p style={{ color: content }}>Blog in progress</p>
-												</div>
-											</div>
-					
-										</div>
-								</footer>
-							</Page.Footer>
-						</Page>
-					)} 
-				}
-			</ThemeContext.Consumer>
-		);
-	}
+                  </article>
+                </main>
+              </Page.Body>
+              <Page.Footer className={styles.dPageFooter}>
+                <footer>
+                  <Seperator />
+
+                  <HeaderR text={'SpaceH'} onClick={handleBlogName} />
+                    <div className={styles.footerMainContainer}>
+
+                      <div className={styles.footerLeftContainer}>
+                        <Avatar image={require('../../assets/me.jpg')} size={'60px'}/>
+                      </div>
+
+                      <div className={styles.footerRightContainer}>
+                        <div className={styles.footerTopTextStyle}>
+                          <p style={{ color: content }}>
+                            Personal Blog by <Anchor href={''} style={{ color: link }} text={'Harshit Prajapati'} />
+                          </p>
+                          <p style={{ color: content }}>Blog in progress</p>
+                        </div>
+                      </div>
+
+                  </div>
+                </footer>
+              </Page.Footer>
+            </Page>
+          )} 
+        }
+      </ThemeContext.Consumer>
+    );
+  }
 }
 
 DefaultPostLayout.propTypes = {
-	postTitle: PropTypes.string,
-	postDate: PropTypes.string,
-	size: PropTypes.string,
-	mainContent: PropTypes.object,
+  postTitle: PropTypes.string,
+  postDate: PropTypes.string,
+  size: PropTypes.string,
+  mainContent: PropTypes.object,
 };
 
 DefaultPostLayout.defaultProps = {
-	postTitle: 'You shouldn`t be seeing this',
-	postDate: '9th April, 1995',
-	size: '70px',
-	mainContent: {},
+  postTitle: 'You shouldn`t be seeing this',
+  postDate: '9th April, 1995',
+  size: '70px',
+  mainContent: {},
 };
 
 export default DefaultPostLayout;
